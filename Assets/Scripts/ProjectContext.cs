237f9f1c6750851860d40a9a3f77a5ccde90b38f
Teppo Kauppinen <eimink@gmail.com>
2015-01-28 21:11:54 +0200

    Initial version

---
+++ Assets/Scripts/ProjectContext.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using strange.extensions.context.api;
+using strange.extensions.context.impl;
+using strange.extensions.dispatcher.eventdispatcher.api;
+using strange.extensions.dispatcher.eventdispatcher.impl;
+
+namespace Project
+{
+	public class ProjectContext : MVCSContext {
+
+		public ProjectContext (MonoBehaviour view) : base(view)
+		{
+		}
+
+		public ProjectContext (MonoBehaviour view, ContextStartupFlags flags) : base(view, flags)
+		{
+		}
+		
+		protected override void mapBindings()
+		{
+			// Injector, mediator and command bindings go here
+		}
+	}
+}
---
+++ Assets/Scripts/ProjectRoot.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using strange.extensions.context.impl;
+
+namespace Project
+{
+	public class ProjectRoot : ContextView {
+
+		void Awake()
+		{
+			context = new ProjectContext(this);
+		}
+	}
+}
